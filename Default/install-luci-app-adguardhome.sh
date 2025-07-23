c88eafd643a80356933a126d4452fd3151cb8265
Aaron Krawczyk <aaron.krawczyk@cit.ie>
2025-07-23 10:19:47 +0100

    Update install-luci-app-adguardhome.sh

--- Default/install-luci-app-adguardhome.sh
+++ Default/install-luci-app-adguardhome.sh
@@ -12,20 +12,20 @@ SRC_DIR="$(pwd)"  # Change if needed
 
 # 1. Install LuCI view .js files
 mkdir -p /www/luci-static/resources/view/adguardhome/
-cp "$SRC_DIR/htdocs/"*.js /www/luci-static/resources/view/adguardhome/
+cp "$SRC_DIR/www/luci-static/resources/view/adguardhome/"*.js /www/luci-static/resources/view/adguardhome/
 
 # 2. Install RPCD backend script
 mkdir -p /usr/libexec/rpcd/
-cp "$SRC_DIR/files/luci.adguardhome" /usr/libexec/rpcd/
+cp "$SRC_DIR/usr/libexec/rpcd/luci.adguardhome" /usr/libexec/rpcd/
 chmod +x /usr/libexec/rpcd/luci.adguardhome
 
 # 3. Install LuCI menu definition
 mkdir -p /usr/share/luci/menu.d/
-cp "$SRC_DIR/files/luci-menu.d.json" /usr/share/luci/menu.d/luci-app-adguardhome.json
+cp "$SRC_DIR/usr/share/luci/menu.d/luci-app-adguardhome.json" /usr/share/luci/menu.d/
 
 # 4. Install RPCD ACL (access control)
 mkdir -p /usr/share/rpcd/acl.d/
-cp "$SRC_DIR/files/rcp-acl.d.json" /usr/share/rpcd/acl.d/luci-app-adguardhome.json
+cp "$SRC_DIR/usr/share/rpcd/acl.d/luci-app-adguardhome.json" /usr/share/rpcd/acl.d/
 
 echo "Restarted rpcd and reload LuCI:"
 /etc/init.d/rpcd restart
