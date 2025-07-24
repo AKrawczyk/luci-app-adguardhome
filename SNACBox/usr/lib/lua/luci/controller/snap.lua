e2e9d48a2f80ecea59821992c23229c4929b807e
Aaron Krawczyk <aaron.krawczyk@cit.ie>
2025-07-24 11:14:14 +0100

    Fixed menu names

--- SNACBox/usr/lib/lua/luci/controller/snap.lua
+++ SNACBox/usr/lib/lua/luci/controller/snap.lua
@@ -5,13 +5,15 @@ function index()
     entry({"admin", "snac"}, firstchild(), "Web Guard", 1).acl_depends = { "luci-app-openvpn" }
 
     -- AdGuard Home submenu under SNAC
-    entry({"admin", "snac", "adguardhome"}, firstchild(), "AdGuard Home", 2).acl_depends = { "luci-app-adguardhome" }
+    entry({"admin", "snac", "adguardhome"}, firstchild(), "ADGuardHome", 2).acl_depends = { "luci-app-adguardhome" }
     entry({"admin", "snac", "adguardhome", "dashboard"}, view("adguardhome/dashboard"), "Dashboard", 1).leaf = true
     entry({"admin", "snac", "adguardhome", "status"}, view("adguardhome/status"), "Status", 11).leaf = true
     entry({"admin", "snac", "adguardhome", "logs"}, view("adguardhome/logs"), "Logs", 21).leaf = true
     entry({"admin", "snac", "adguardhome", "config"}, view("adguardhome/config"), "Configuration", 31).leaf = true
 
     -- Device and Schedule submenu under SNAC
-    entry({"admin", "snac", "deviceaccess"}, template("snac"), "Device Access", 3).leaf = true
-    entry({"admin", "snac", "scheduleaccess"}, template("schedulesnac"), "Schedule Access", 4).leaf = true
+    entry({"admin", "snac", "webguard"}, firstchild(), "Web Access", 3).acl_depends = { "luci-app-adguardhome" }
+    entry({"admin", "snac", "webguard", "deviceaccess"}, template("snac"), "Device Access", 1).leaf = true
+    entry({"admin", "snac", "webguard", "scheduleaccess"}, template("schedulesnac"), "Schedule Access",11).leaf = true
+    entry({"admin", "snac", "webguard", "config"}, view("config"), "Configuration", 21).leaf = true
 end
